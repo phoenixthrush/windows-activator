68d0410c4fb86919137ea15f88d76aeef014109c
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-02-25 11:03:39 +0100

    add more license keys

--- src/common.c
+++ src/common.c
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <stdio.h>
 
 int is_admin()
 {
--- src/common.h
+++ src/common.h
@@ -2,6 +2,7 @@
 #define COMMON_H
 
 #include <windows.h>
+#include <stdio.h>
 
 int is_admin();
 void run_command(const char *command);
