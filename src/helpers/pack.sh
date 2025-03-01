68329d7770399836dd0e93c5cafa621c20eb2537
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-03-01 16:56:50 +0100

    add commit hash to filename

--- src/helpers/pack.sh
+++ src/helpers/pack.sh
@@ -2,6 +2,7 @@
 
 cross_build="build/bin/Release/activator.exe"
 build="build/bin/activator"
+commit_hash=$(git rev-parse --short HEAD)
 
 if [ -f "$cross_build" ] || [ -f "$build" ]; then
     echo "Activator found, compressing with UPX..."
@@ -9,11 +10,13 @@ if [ -f "$cross_build" ] || [ -f "$build" ]; then
     if which upx &>/dev/null; then
         if [ -f "$cross_build" ]; then
             upx --best "$cross_build"
+            mv "$cross_build" "build/bin/Release/activator.$commit_hash.exe"
         elif [ -f "$build" ]; then
             if [[ "$(uname)" == "Darwin" ]]; then
                 echo "Skipping compression on macOS, only cross builds are compressed."
             else
                 upx --best "$build"
+                mv "$build" "build/bin/activator.$commit_hash"
             fi
         fi
     else
