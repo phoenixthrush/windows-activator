38b5a3b70af7d25d515546431c413a87be5e3959
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-03-01 17:24:09 +0100

    skip upx compression on arm based machines

--- src/helpers/pack.ps1
+++ src/helpers/pack.ps1
@@ -3,7 +3,13 @@ $activatorPath = "build/bin/activator.exe"
 if (Test-Path $activatorPath) {
     Write-Host "$activatorPath found, compressing with UPX..."
     if (Get-Command upx -ErrorAction SilentlyContinue) {
-        upx --best $activatorPath
+        $processorArchitecture = [System.Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")
+
+        if ($processorArchitecture -eq "ARM64") {
+            Write-Host "Skipping UPX compression as the system is ARM-based."
+        } else {
+            upx --best $activatorPath
+        }
     } else {
         Write-Host "UPX is not installed."
     }
