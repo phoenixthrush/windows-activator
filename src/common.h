6b67af648f38da1118a48a699cbcc355abff33c2
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-03-02 18:47:20 +0100

    add bindings on non windows

--- src/common.h
+++ src/common.h
@@ -1,18 +1,22 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <windows.h>
 #include <stdio.h>
-#include <urlmon.h>
+#include <stdlib.h>
 #include "webview/webview.h"
 
+void credits_cb(const char *seq, const char *req, void *arg);
+void quit_cb(const char *seq, const char *req, void *arg);
+
+#ifdef _WIN32
+#include <windows.h>
+#include <urlmon.h>
 #pragma comment(lib, "urlmon.lib")
 
 int is_admin();
 void run_command(const char *command);
 int download_file(const char *url, const char *destination);
 int extract_tar(const char *tar_file, const char *target_dir);
-void credits_cb(const char *seq, const char *req, void *arg);
-void quit_cb(const char *seq, const char *req, void *arg);
+#endif
 
 #endif // COMMON_H
