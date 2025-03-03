81db020f486ca6a24e81a7fdacde4e909dc12167
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-03-03 20:34:08 +0100

    dont pack if on arm64

--- src/helpers/pack.ps1
+++ src/helpers/pack.ps1
@@ -4,20 +4,17 @@ if (Test-Path $activatorPath) {
     Write-Host "$activatorPath found, compressing with UPX..."
     if (Get-Command upx -ErrorAction SilentlyContinue) {
         $processorArchitecture = [System.Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")
+        $processorIdentifier = [System.Environment]::GetEnvironmentVariable("PROCESSOR_IDENTIFIER")
 
-        if ($processorArchitecture -eq "ARM64") {
+        if ($processorArchitecture -eq "ARM64" -or $processorIdentifier -match "ARM" -or $processorIdentifier -match "AArch64") {
             Write-Host "Skipping UPX compression as the system is ARM-based."
         } else {
+            Write-Host "Running UPX compression..."
             upx --best $activatorPath
         }
     } else {
         Write-Host "UPX is not installed."
     }
-
-    $commit_hash = git rev-parse --short HEAD
-    $newFileName = "build/bin/activator.$commit_hash.exe"
-    Rename-Item $activatorPath $newFileName
-    Write-Host "File renamed to $newFileName"
 } else {
     Write-Host "$activatorPath not found."
 }
