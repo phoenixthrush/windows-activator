1ae85f7dbde55bc0db6dddf10895321eb40742e8
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-03-04 00:28:14 +0100

    link to a stylesheet file

--- src/helpers/modify.py
+++ src/helpers/modify.py
@@ -3,6 +3,10 @@ import base64
 import os
 import argparse
 
+# pip install minify-html
+# https://docs.rs/minify-html/latest/minify_html/struct.Cfg.html
+import minify_html
+
 
 def encode_file_to_base64(filepath):
     if not os.path.isfile(filepath):
@@ -26,8 +30,20 @@ def replace_file_with_base64(match):
     return match.group(0)
 
 
+def replace_stylesheets_with_base64(match):
+    filepath = os.path.join("site", match.group(1))
+    if os.path.isfile(filepath):
+        with open(filepath, "r", encoding="utf-8") as f:
+            css_content = f.read()
+        return f'<style>{css_content}</style>'
+    print(f"Warning: CSS file '{filepath}' not found. Skipping embedding.")
+    return match.group(0)
+
+
 def modify_content(content):
-    return re.sub(r'src="([^"]+)"', replace_file_with_base64, content)
+    content = re.sub(r'src="([^"]+)"', replace_file_with_base64, content)
+    content = re.sub(r'<link rel="stylesheet" href="([^"]+)">', replace_stylesheets_with_base64, content)
+    return minify_html.minify(content, minify_css=True, minify_js=True)
 
 
 def process_html_file(input_filename, output_filename):
@@ -40,10 +56,8 @@ def process_html_file(input_filename, output_filename):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Convert file references in HTML to base64 data URIs.")
+    parser = argparse.ArgumentParser(description="Convert file references in HTML to base64 data URIs and embed stylesheets.")
     parser.add_argument("-i", "--input", required=True, help="The input HTML file to process")
     parser.add_argument("-o", "--output", required=True, help="The output file where modified HTML will be saved")
-
     args = parser.parse_args()
-
     process_html_file(args.input, args.output)
