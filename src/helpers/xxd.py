775bbfe93985388f602af616f9bf3227f2057e07
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-03-04 00:50:47 +0100

    optimise xxd helper

--- src/helpers/xxd.py
+++ src/helpers/xxd.py
@@ -2,33 +2,37 @@ import os
 import argparse
 
 
-def convert(filename, output_filename=None):
-    if output_filename is None:
-        output_filename = os.path.splitext(os.path.basename(filename))[0] + ".c"
-    with open(filename, "rb") as f:
-        data = f.read()
-    c_array_name = os.path.basename(filename).replace(".", "_")
-    hex_values = ["0x{:02x}".format(byte) for byte in data]
-    hex_values.append("0x00")
-    with open(output_filename, "w") as f:
-        f.write("unsigned char {}[] = {{\n".format(c_array_name))
-        for i in range(0, len(hex_values), 12):
-            group = hex_values[i:i+12]
-            f.write("  " + ", ".join(group))
-            if i + 12 < len(hex_values):
-                f.write(",\n")
-            else:
-                f.write("\n")
-        f.write("};\n")
-        f.write("unsigned int {}_len = {};\n".format(c_array_name, len(data) + 1))
-        print(f"Saved as ./{output_filename}")
+def convert_file_to_c_array(input_file, output_file=None):
+    if output_file is None:
+        output_file = os.path.splitext(os.path.basename(input_file))[0] + ".c"
+    with open(input_file, "rb") as file_handle:
+        file_bytes = file_handle.read()
+    c_array_name = os.path.basename(input_file).replace(".", "_")
+    hex_byte_strings = [
+        "0x{:02x}".format(byte) for byte in file_bytes
+    ] + ["0x00"]
+    lines = [
+        "  " + ", ".join(hex_byte_strings[i:i+12])
+        for i in range(0, len(hex_byte_strings), 12)
+    ]
+    output_content = "\n".join(
+        [
+            f"unsigned char {c_array_name}[] = {{",
+            ",\n".join(
+                lines
+            ), "};", f"unsigned int {c_array_name}_len = {len(file_bytes) + 1};"
+        ]
+    )
+    with open(output_file, "w") as out_handle:
+        out_handle.write(output_content + "\n")
+    print(f"Saved as ./{output_file}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert file to C array.")
-    parser.add_argument("-i", "--input", required=True, help="The input file to convert")
-    parser.add_argument("-o", "--output", help="The output C file (default is input file name with .c extension)")
-
+    parser.add_argument("-i", "--input", required=True,
+                        help="The input file to convert")
+    parser.add_argument(
+        "-o", "--output", help="The output C file (default is input file name with .c extension)")
     args = parser.parse_args()
-
-    convert(args.input, args.output)
+    convert_file_to_c_array(args.input, args.output)
